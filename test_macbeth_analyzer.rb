2de28246967d957a2b94fe74c7885423bbb6bdb1
jasonklein <jasonklein@gmail.com>
2014-08-01 11:04:28 +0100

    Revised test file to take advantage of RSpec-like functionality in Minitest.

--- test_macbeth_analyzer.rb
+++ test_macbeth_analyzer.rb
@@ -1,25 +1,52 @@
 require 'minitest/autorun'
+
+
 require_relative 'macbeth_analyzer'
 
-class LinesKeeperTest < MiniTest::Unit::TestCase
+describe LinesKeeper do
 
-  attr_reader :lines_keeper
+  let(:lines_keeper) {LinesKeeper.new('http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml')}
 
-  def setup
-    @lines_keeper = LinesKeeper.new('http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml')
+  describe '.new' do
+    it 'creates a LinesKeeper object' do
+      lines_keeper.wont_be_nil
+    end
+
+    it 'is initialized with a url' do
+      lines_keeper.url.must_equal 'http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml'
+    end
   end
 
-  def test_url
-    lines_keeper.url.must_equal 'http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml'
-  end
-
-  def test_file_to_parse
-    lines_keeper.get_file
-    lines_keeper.file_to_parse.must_be_kind_of String
-  end
-
-  def test_break_into_speeching
-    lines_keeper.break_into_speechings
-    lines_keeper.speechings.must_be_kind_of Array
+  describe '#get_file' do
+    it 'gets the file from the url' do
+      lines_keeper.get_file
+      lines_keeper.file_to_parse.must_be_kind_of String
+    end
   end
 end
+
+# attr_reader :lines_keeper
+
+# def setup
+#   @lines_keeper = LinesKeeper.new('http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml')
+# end
+
+# before do
+#   @lines_keeper = LinesKeeper.new('http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml')
+# end
+
+
+
+# def test_url
+#   lines_keeper.url.must_equal 'http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml'
+# end
+
+# def test_file_to_parse
+#   lines_keeper.get_file
+#   lines_keeper.file_to_parse.must_be_kind_of String
+# end
+
+# def test_break_into_speeching
+#   lines_keeper.break_into_speechings
+#   lines_keeper.speechings.must_be_kind_of Array
+# end
