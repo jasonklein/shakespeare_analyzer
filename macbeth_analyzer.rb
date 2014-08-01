8e8e7a0c3e000ceb670426ff91193bd55d2f914c
jasonklein <jasonklein@gmail.com>
2014-08-01 11:52:37 +0100

    Move remaining functions into LinesKeeper class; add tests for those functions; delete commented out code.

--- macbeth_analyzer.rb
+++ macbeth_analyzer.rb
@@ -2,44 +2,50 @@ require 'open-uri'
 
 class LinesKeeper
 
-  attr_reader :url, :file_to_parse, :speechings
+  attr_reader :url, :file_to_parse, :speechings, :characters_and_lines
 
   def initialize(url)
     @url = url
+    @characters_and_lines = Hash.new(0)
   end
 
   def get_file
     @file_to_parse = open(url).read
   end
 
-  def break_into_speechings
+  def break_file_into_speechings
     @speechings = file_to_parse.split('SPEECH>')
   end
 
-end
+  def determine_characters_and_lines
+    speaker_name_start = '<SPEAKER>'
+    speaker_name_end = '</SPEAKER>'
 
-url = 'http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml'
-
-macbeth = open(url).read
-macbeth_speechings = macbeth.split('SPEECH>')
-characters_and_lines = Hash.new(0)
-speaker_name_start = '<SPEAKER>'
-speaker_name_end = '</SPEAKER>'
-
-macbeth_speechings.each do |speech|
-  if speech.include?(speaker_name_start) && speech.include?(speaker_name_end)
-    speaker = speech[/#{Regexp.escape(speaker_name_start)}(.*?)#{Regexp.escape(speaker_name_end)}/m,1]
-    characters_and_lines[speaker.to_sym] += speech.scan("<LINE>").count
+    speechings.each do |speech|
+      if speech.include?(speaker_name_start) && speech.include?(speaker_name_end)
+        speaker = speech[/#{Regexp.escape(speaker_name_start)}(.*?)#{Regexp.escape(speaker_name_end)}/m,1]
+        characters_and_lines[speaker] += speech.scan("<LINE>").count
+      end
+    end
   end
+
+  def print_characters_and_lines
+    characters_and_lines.each do |character,lines|
+      name_parts = character.split(" ")
+      name_parts_capitalized = []
+      name_parts.each { |np| name_parts_capitalized << np.capitalize }
+      character = name_parts_capitalized.join(" ")
+      puts "#{character}: #{lines}"
+    end
+
+    total_lines = characters_and_lines.values.reduce {|sum, n| sum + n.to_i }
+    puts "Total lines: #{total_lines}"
+  end
+
 end
 
-characters_and_lines.each do |character,lines|
-  name_parts = character.to_s.split(" ")
-  name_parts_capitalized = []
-  name_parts.each { |np| name_parts_capitalized << np.capitalize }
-  character = name_parts_capitalized.join(" ")
-  puts "#{character}: #{lines}"
-end
-
-total_lines = characters_and_lines.values.reduce {|sum, n| sum + n.to_i }
-puts "Total lines: #{total_lines}"
+macbeth_analyzer = LinesKeeper.new('http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml')
+macbeth_analyzer.get_file
+macbeth_analyzer.break_file_into_speechings
+macbeth_analyzer.determine_characters_and_lines
+macbeth_analyzer.print_characters_and_lines
--- test_macbeth_analyzer.rb
+++ test_macbeth_analyzer.rb
@@ -15,6 +15,12 @@ describe LinesKeeper do
     it 'is initialized with a url' do
       lines_keeper.url.must_equal 'http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml'
     end
+
+    it 'is initialized with an empty characters_and_lines hash' do
+      lines_keeper.characters_and_lines.must_be_kind_of Hash
+      lines_keeper.characters_and_lines.must_be_empty
+    end
+
   end
 
   describe '#get_file' do
@@ -23,30 +29,30 @@ describe LinesKeeper do
       lines_keeper.file_to_parse.must_be_kind_of String
     end
   end
+
+  describe '#break_file_into_speechings' do
+    it 'splits the file into an array' do
+      lines_keeper.get_file
+      lines_keeper.break_file_into_speechings
+      lines_keeper.speechings.must_be_kind_of Array
+    end
+  end
+
+  describe '#determine_characters_and_lines' do
+    it 'fills the characters_and_lines hash' do
+      lines_keeper.get_file
+      lines_keeper.break_file_into_speechings
+      lines_keeper.determine_characters_and_lines
+      lines_keeper.characters_and_lines.wont_be_empty
+    end
+  end
+
+  describe '#print_characters_and_lines' do
+    it 'prints the characters and lines' do
+      lines_keeper.get_file
+      lines_keeper.break_file_into_speechings
+      lines_keeper.determine_characters_and_lines
+      lines_keeper.print_characters_and_lines
+    end
+  end
 end
-
-# attr_reader :lines_keeper
-
-# def setup
-#   @lines_keeper = LinesKeeper.new('http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml')
-# end
-
-# before do
-#   @lines_keeper = LinesKeeper.new('http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml')
-# end
-
-
-
-# def test_url
-#   lines_keeper.url.must_equal 'http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml'
-# end
-
-# def test_file_to_parse
-#   lines_keeper.get_file
-#   lines_keeper.file_to_parse.must_be_kind_of String
-# end
-
-# def test_break_into_speeching
-#   lines_keeper.break_into_speechings
-#   lines_keeper.speechings.must_be_kind_of Array
-# end
